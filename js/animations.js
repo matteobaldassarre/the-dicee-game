5d4a65b847bba5081d699c90ac2bf6dbe7acc843
matteobaldassarre <matteobaldassarre29@gmail.com>
2021-08-14 20:21:56 +0200

    first commit

---
+++ js/animations.js
@@ -0,0 +1,20 @@
+// GSAP Code
+var timeline = gsap.timeline(); // Creating a timeline to manage sequential animations better
+
+// Fade in/out of the game name heading
+timeline.from("#welcome", {duration: 2, opacity: 0});
+timeline.to("#welcome", {duration: 1, opacity: 0});
+
+
+// Heading slide down
+timeline.from("h1", {duration: 1, opacity: 0, ease: 'expo', y: -800});
+
+
+// Lateral slide of player one column
+timeline.from(".player-one", {duration: 1.5, opacity: 0, ease: 'expo', x: 300});
+// Lateral slide of player two column
+timeline.from(".player-two", {duration: 1.5, opacity: 0, ease: 'expo', x: -300});
+
+
+// Throw button fade in
+timeline.from("#throw-dicee", {duration: 0.5, opacity: 0});
---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Function that contains all the functionalities of the game
+function startGame() {
+    // Storing a random number from 1 to 6 for the Player One
+    let randomNumber1 = getRandomNumber();
+
+    // Storing a random number from 1 to 6 for the Player Two
+    let randomNumber2 = getRandomNumber();
+
+
+    // Selecting the Player One dice-image and changing its src attribute based on the random number
+    let playerOneDice = document.querySelector('.player-one img');
+    let playerOneDiceValue = 'img/dice' + randomNumber1 + '.png';
+    playerOneDice.setAttribute('src', playerOneDiceValue);
+
+
+    // Selecting the Player Two dice-image and changing its src attribute based on the random number
+    let playerTwoDice = document.querySelector('.player-two img');
+    let playerTwoDiceValue = 'img/dice' + randomNumber2 + '.png';
+    playerTwoDice.setAttribute('src', playerTwoDiceValue);
+
+
+    // Selecting the result heading element from the DOM by its id
+    let resultMessage = document.getElementById('result');
+
+    // Conditional statement to determine the winner
+    if (randomNumber1 === randomNumber2) {
+        fadeResult();
+        resultMessage.innerHTML = 'Draw!';
+    } else if (randomNumber1 > randomNumber2) {
+        fadeResult();
+        resultMessage.innerHTML = 'Player One Wins!';
+    } else {
+        fadeResult();
+        resultMessage.innerHTML = 'Player Two Wins!';
+    }
+
+    document.getElementById('throw-dicee').innerHTML = 'Throw Again';
+}
+
+
+// Making the title fade after the result
+function fadeResult() {
+    gsap.from("h1", {
+        duration: 1,
+        opacity: 0,
+    });
+}
+
+
+// Function that generates a random number
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    return randomNumber;
+}
